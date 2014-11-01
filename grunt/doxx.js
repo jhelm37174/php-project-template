fa7728200e6227f6c27ae98926368d7965bd8d73
jhelm37174 <jamiehelm@hotmail.com>
2014-11-01 17:39:11 +0700

    added php and js documentation grunt tasks and config files

---
+++ grunt/doxx.js
@@ -0,0 +1,14 @@
+module.exports = function (grunt) {
+  return {
+
+    all: {
+      src: 'htdocs/src',
+      target: 'documentation/js',
+      options: {
+        title: 'Project Java Script Documentation'
+      }
+    }
+
+
+  };
+};
---
+++ grunt/phpdocumentor.js
@@ -0,0 +1,12 @@
+module.exports = function (grunt) {
+  return {
+
+    dist: {
+      options: {
+        directory: 'htdocs',
+        target: 'documentation/php'
+      }
+    }
+
+  };
+};
