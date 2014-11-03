dcb97fa23da6a3d3d8e7f02446e205898c75e325
James Helm <jameshelm@venda.com>
2014-11-03 16:30:44 +0700

    fixed name of test cases so phpunit can find them

---
+++ specs/php/exampleTest.php
@@ -0,0 +1,20 @@
+<?php
+class BeautifyTest extends PHPUnit_Framework_TestCase
+{
+  public function test_exampleFunction()
+  {
+    $this->assertTrue(exampleFunction(1,2,3) == 6);
+  }
+
+  public function test_exampleFunction2()
+  {
+    $this->assertTrue(exampleFunction(5,5,5) == 15);
+  }
+
+  public function test_exampleFunction3()
+  {
+    $this->assertTrue(exampleFunction(1,1,1) == 3);
+  }
+
+}
+
